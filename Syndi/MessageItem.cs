a1947e7e3cd0c190a85e9a61fe2011b7d0fb810d
RoboDoc <theonlyrobodoc@gmail.com>
2024-02-27 13:17:13 +0200

    Created class to hold some message data

---
+++ Syndi/MessageItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Syndi
+{
+    internal class MessageItem(string channelID, string messageID)
+    {
+        public string ChannelID { get; private set; } = channelID;
+        public string MessageID { get; private set; } = messageID;
+    }
+}
